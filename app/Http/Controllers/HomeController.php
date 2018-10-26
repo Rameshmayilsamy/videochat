c84e51e964f9de2edabcf08db794f5578498c372
Rameshmayilsamy <43106277+Rameshmayilsamy@users.noreply.github.com>
2018-10-26 17:03:25 +0530

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \Pusher\Pusher;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+
+        $pusher = new Pusher('c66ec9ec875d5f11488a', '8973fedc45b74d432fd9', '632135', [
+            'cluster' => 'ap2',
+            'useTLS' => true
+        ]);
+
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+
+        return response($key);
+    }
+}
---
+++ storage/framework/views/9a6f399e519bb7ed7c513d89d546d58da2859c8a.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+<div id="app">
+    
+
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
